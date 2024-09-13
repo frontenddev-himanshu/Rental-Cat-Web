10f01a3c5a2afdc2b8fa1b37bc34ea201570c852
frontenddev-himanshu <rajh5343@gmail.com>
2024-09-13 19:51:24 +0530

    new project js

---
+++ script.js
@@ -0,0 +1,24 @@
+// cdn link
+AOS.init();
+
+
+
+
+
+// // navbar
+// const menuBtn = document.getElementById("menu-btn");
+// const navLinks = document.getElementById("nav-links");
+// const menueBtnIcon = menuBtn.querySelector("i");
+
+// menuBtn.addEventListener("click", (e) => {
+//     navLinks.classList.toggle("open");
+
+//     const isOpen = navLinks.classList.contains("open");
+//     menueBtnIcon.setAttribute("class", isOpen ? "ri-menu-line" : "ri-menu-line");
+// })
+
+// navLinks.addEventListener("click", (e) => {
+//     navLinks.classList.remove("open");
+//     menueBtnIcon.setAttribute("class", "ri-menu-line");
+// });
+
